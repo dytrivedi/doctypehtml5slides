8f0f1282229d78d2351ac4496f4b0eb033aee34b
Dhaval Trivedi <trivedi.dhaval@gmail.com>
2010-10-05 13:59:39 +0530

    Removed some browser specific effects and added IE9 background. Also fixed the JS and the code is now more OO.

--- js/script.js
+++ js/script.js
@@ -1,65 +1,74 @@
 jQuery(document).ready(function() {
-    var currentSlide = 0;
-    var slideCount = jQuery('.slides > .slide').length;
-    jQuery(document).keydown(function(e) {
-        // left
-        if (e.keyCode == '37') {
-
-            if (currentSlide == 0) {
+    var slideShow = {
+        currentSlide: 0,
+        slideCount: jQuery('.slides > .slide').length,
+        delay: 1000,
+        animating: false,
+        movePrev: function() {
+            if (this.currentSlide == 0 || this.animating) {
                 return;
             }
-
+            this.animating = true;
             jQuery('.slides .far-past').eq(jQuery('.far-past').length - 1).animate ({
                 marginLeft: '-1400px'
-            },500,'linear',function() {
+            },this.delay,'swing',function() {
                 jQuery(this).removeClass('far-past').addClass('past');
             });
             jQuery('.slides .past').animate ({
                 marginLeft: '-450px'
-            },500,'linear',function() {
+            },this.delay,'swing',function() {
                 jQuery(this).removeClass('past').addClass('current');
             });
             jQuery('.slides .current').animate ({
                 marginLeft: '500px'
-            },500,'linear',function() {
+            },this.delay,'swing',function() {
                 jQuery(this).removeClass('current').addClass('future');
+                slideShow.animating = false;
             });
             jQuery('.slides .future').animate ({
                 marginLeft: '1500px'
-            },500,'linear',function() {
+            },this.delay,'swing',function() {
                 jQuery(this).removeClass('future').addClass('far-future');
+                slideShow.animating = false;
             });
-            currentSlide--;
-
-        // right
-        } else if (e.keyCode == '39' || e.keyCode == '32') {
-
-            if (currentSlide == slideCount - 1) {
+            this.currentSlide--;
+        },
+        moveNext: function() {
+            if (this.currentSlide == this.slideCount - 1 || this.animating) {
                 return;
             }
-
+            this.animating = true;
             jQuery('.slides .past').animate ({
                 marginLeft: '-2400px'
-            },500,'linear',function() {
+            },this.delay,'swing',function() {
                 jQuery(this).removeClass('past').addClass('far-past');
             });
             jQuery('.slides .current').animate ({
                 marginLeft: '-1400px'
-            },500,'linear',function() {
+            },this.delay,'swing',function() {
                 jQuery(this).removeClass('current').addClass('past');
+                slideShow.animating = false;
             });
             jQuery('.slides .future').animate ({
                 marginLeft: '-450px'
-            },500,'linear',function() {
+            },this.delay,'swing',function() {
                 jQuery(this).removeClass('future').addClass('current');
             });
             jQuery('.slides .far-future:eq(0)').animate ({
                 marginLeft: '500px'
-            },500,'linear',function() {
+            },this.delay,'swing',function() {
                 jQuery(this).removeClass('far-future').addClass('future');
             });
-            currentSlide++;
-
+            this.currentSlide++;
+        }
+    }
+    jQuery(document).keydown(function(e) {
+        // left
+        if (e.keyCode == '37') {
+            slideShow.movePrev();
+        // right
+        } else if (e.keyCode == '39' || e.keyCode == '32') {
+            slideShow.moveNext();
         }
     });
 });
